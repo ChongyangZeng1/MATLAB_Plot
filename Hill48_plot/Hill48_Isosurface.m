a76e5db7e139844307458e5fc76f6b22716a4292
MaxB <max.bisch@uni-siegen.de>
2023-03-20 16:24:31 +0100

    Modified both Isosurfaces
    Added shear-dependence to the N1 parameter and some vmises comparison

--- Hill48_plot/Hill48_Isosurface.m
+++ Hill48_plot/Hill48_Isosurface.m
@@ -11,6 +11,8 @@ Sig65 = 349.1;
 Sig75 = 356.1;
 Sig90 = 359.4;
 Sigb = 345;
+Tau = 330;
+Tau = Tau/Sig00;
 
 
 Sig45 =Sig45/Sig00;
@@ -18,11 +20,17 @@ Sig90 =Sig90/Sig00;
 Sigb =Sigb/Sig00;
 Sig00 = 1;
 % Calculate F1, G1, and H1 coefficients
-F1 = (Sig00/Sig90)^2 - 1 + (Sig00/Sigb)^2;
-G1 = 1 - (Sig00/Sig90)^2 + (Sig00/Sigb)^2;
-H1 = 1 + (Sig00/Sig90)^2 - (Sig00/Sigb)^2;
-N1 =  (4*(Sig00)^2)/(Sig45)^2 - ((Sig00)^2)/((Sigb)^2);
-N1=3;
+% F1 = (Sig00/Sig90)^2 - 1 + (Sig00/Sigb)^2;
+F1 = 1;
+% G1 = 1 - (Sig00/Sig90)^2 + (Sig00/Sigb)^2;
+G1 = 1;
+% H1 = 1 + (Sig00/Sig90)^2 - (Sig00/Sigb)^2;
+H1 = 1;
+% N1 =  (4*(Sig00)^2)/(Sig45)^2 - ((Sig00)^2)/((Sigb)^2);
+% N1 = (4*(Sig00)^2)/(Sig45)^2 - ((Sig00)^2 + 2*Tau^2)/((Sigb)^2);
+% N1 = (4*(Sig00)^2)/(Sig45)^2 - ((Sig00)^2 + Tau^2)/((Sigb)^2);
+N1 = 3;
+
 % Range of stress components
 sigma1_range = linspace(0, 2, 100);
 sigma2_range = linspace(-2, 2, 100);
@@ -34,16 +42,32 @@ tau12_range = linspace(0, 2, 100);
 % Calculate the Hill48 yield function value for plane stress conditions (sigma3 = 0)
 hill48 = (G1  * (sigma1.^2) + F1  * (sigma2.^2) +  H1 * (sigma1 - sigma2).^2)/2 + N1*(tau12.^2);
 
-% Find the surface where Hill48 yield function equals 1
-isosurface_threshold = 1;
+% Calculate the von Mises yield criterion value for plane stress conditions (sigma3 = 0)
+vonMises = sqrt(.5*((sigma1 - sigma2).^2 + sigma2.^2 + sigma1.^2) + 3 * tau12.^2);
 
 % Create a 3D surface plot
 figure;
-isosurface(sigma1, sigma2, tau12, hill48, isosurface_threshold);
+h1 = patch(isosurface(sigma1, sigma2, tau12, hill48, isosurface_threshold));
+set(h1, 'FaceColor', 'b', 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+hold on;
+h2 = patch(isosurface(sigma1, sigma2, tau12, vonMises, isosurface_threshold));
+set(h2, 'FaceColor', 'r', 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+
 xlabel('Stress Component 1 (\sigma_1)');
 ylabel('Stress Component 2 (\sigma_2)');
 zlabel('Shear Stress (\tau_{12})');
-title('3D Surface Plot of Hill48 Plasticity Model under Plane Stress Conditions');
+title('3D Surface Plot of Hill48 Plasticity Model and von Mises Yield Criterion under Plane Stress Conditions');
 axis equal;
 grid on;
 view(3);
+
+% Experimental results from the pure shear test
+experimental_sigma1 = 0; % Replace with your experimental value
+experimental_sigma2 = 0; % Replace with your experimental value
+experimental_tau12 = Tau*.6; % Replace with your experimental value
+
+% Plot the experimental point on the 3D surface
+hold on;
+plot3(experimental_sigma1, experimental_sigma2, experimental_tau12, 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
+legend('Hill48 Yield Surface', 'Experimental Result');
+hold off;
--- Hill48_plot/Hill90_Isosurface.m
+++ Hill48_plot/Hill90_Isosurface.m
@@ -11,10 +11,13 @@ Sig65 = 349.1;
 Sig75 = 356.1;
 Sig90 = 359.4;
 Sigb = 345;
+Tau = 330;
+
 
 Sig45 =Sig45/Sig00;
 Sig90 =Sig90/Sig00;
 Sigb =Sigb/Sig00;
+Tau = Tau/Sig00;
 Sig00 = 1;
 % Range of stress components
 sigma1_range = linspace(0, 2, 100);
@@ -25,24 +28,23 @@ tau12_range = linspace(0, 2, 100);
 [x, y, tau12] = meshgrid(sigma1_range, sigma2_range, tau12_range);
 
 % Calculate the Hill48 yield function value for plane stress conditions (sigma3 = 0)
-tau =330;
 m=2;
 c=Sigb/Tau;
 a = 0.25*((2*Sigb/Sig90)^m-(2*Sigb/Sig00)^m);
 b = 0.5*((2*Sigb/Sig00)^m+(2*Sigb/Sig90)^m)-(2*Sigb/Sig45)^m;
 
-hill90=(x+y)^m+((c)^m)*(x-y)^m+((x^2+y^2)^(0.5*m-1))*(-2*a*(x^2-y^2)+b*(x-y)^2);
+hill90=(x+y).^m+((c).^m).*(x-y).^m+((x.^2+y.^2).^(0.5*m-1)).*(-2.*a.*(x.^2-y.^2)+b.*(x-y).^2);
 
 % Find the surface where Hill48 yield function equals 1
 isosurface_threshold = 1;
 
 % Create a 3D surface plot
 figure;
-isosurface(sigma1, sigma2, tau12, hill48, isosurface_threshold);
+isosurface(sigma1, sigma2, tau12, hill90, isosurface_threshold);
 xlabel('Stress Component 1 (\sigma_1)');
 ylabel('Stress Component 2 (\sigma_2)');
 zlabel('Shear Stress (\tau_{12})');
-title('3D Surface Plot of Hill48 Plasticity Model under Plane Stress Conditions');
+title('3D Surface Plot of Hill90 Plasticity Model under Plane Stress Conditions');
 axis equal;
 grid on;
 view(3);
